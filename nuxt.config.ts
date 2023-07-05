6cfbffdd4808f5fcb97d38d1376376e786024181
cmm <89752605+GitMac32@users.noreply.github.com>
2023-07-05 08:38:44 -0600

    Update nuxt.config.ts
    added router for GHP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default ({
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt'],
+    router: {
+      base: '/onelink/'
+    },
     build: {
         transpile: ["@headlessui/vue"],
     },
